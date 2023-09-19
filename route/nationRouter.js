59bc9ce79b4ca8e0ad76ba07391eec8a63d8cb67
cubi-dev <cubiiwork@gmail.com>
2023-09-20 03:13:17 +0700

    route

---
+++ route/nationRouter.js
@@ -0,0 +1,182 @@
+const express = require('express')
+const fs = require('fs')
+// ROUTE FOR NATION
+const nationRouter = express.Router(); 
+
+//Convert to JS Obj (blocking|first time)
+const nations = JSON.parse(fs.readFileSync(`${__dirname}/../data/nations.json`));
+
+// NATIONS
+const getAllNation = (req, res) => {
+    // Jsend
+    res.status(200).json({
+      status: "success",
+      results: nations.length,
+      data: {
+        nations: nations,
+      },
+    });
+  };
+  const getNationById = (req, res) => {
+    console.log(req.params);
+    // 1. Convert id to number
+    const id = req.params.id * 1;
+    // 2. Find in tours data
+    const nation = nations.find((el) => el.id === id);
+    // Err Handle
+    if (!nation) {
+      return res.status(404).json({
+        status: "fail",
+        message: "Invalid ID",
+      });
+    }
+    // Success
+    res.status(200).json({
+      status: "success",
+      data: {
+        nations: nation,
+      },
+    });
+  };
+  const createNation = (req, res) => {
+    //  1. CREATE NEW OBJECT
+    //because we dont have db to automatically add newId
+    const newId = nations[nations.length - 1].id + 1;
+    //newTour(body.req) | Object.assign(to create a new object by merging two existing objects)
+    const newNation = Object.assign({ id: newId }, req.body);
+    // 2. PUT THE NEW OBJECT IN TO ARRAY OBJECT
+    nations.push(newNation);
+    // 3. WRITE IT TO FILE (non-blocking)
+    fs.writeFile(
+      `${__dirname}/../data/nations.json`,
+      JSON.stringify(nations),
+      (err) => {
+        //201 - created
+        res.status(201).json({
+          status: "success",
+          data: {
+            nations: newNation,
+          },
+        });
+      }
+    );
+  };
+  const updateNationPatch = (req, res) => {
+    // Find id
+    const id = req.params.id * 1;
+    const nationToUpdate = nations.find((el) => el.id === id);
+    // Err handle
+    if (!nationToUpdate) {
+      res.status(404).json({
+        status: "fail",
+        message: "No tour object with" + id + "is not found",
+      });
+    }
+    // Find index correspond with nation's id in data
+    const nationIndex = nations.indexOf(nationToUpdate);
+    // Update the nation object directly
+    Object.assign(nationToUpdate, req.body);
+    // Update the array with the modified object
+    nations[nationIndex] = nationToUpdate;
+    fs.writeFile(
+      `${__dirname}/../data/nations.json`,
+      JSON.stringify(nations),
+      (err) => {
+        res.status(200).json({
+          status: "success",
+          data: {
+            nations: nationToUpdate,
+          },
+        });
+      }
+    );
+  };
+  const updateNationPut = (req, res) => {
+    res.statusCode = 403;
+    res.end("PUT operation not supported on /nations");
+  };
+  const deleteAllNations = (req, res) => {
+      // Clear the nations array
+      nations.length = 0;
+      // Update the JSON file with an empty array
+      fs.writeFile(
+        `${__dirname}/../data/nations.json`,
+        JSON.stringify(nations),
+        (err) => {
+          if (err) {
+            // Handle any errors that may occur during file write
+            res.status(500).json({
+              status: "error",
+              message: "Failed to delete all nations",
+            });
+          } else {
+            // Success response
+            res.status(203).json({
+              status: "success",
+              data: {
+                message: "All nations deleted successfully",
+              },
+            });
+          }
+        }
+      );
+  }
+  const deleteNationById = (req, res) => {
+      try {
+        // Check if nations is defined and initialize it if necessary
+        if (!Array.isArray(nations)) {
+          nations = [];
+        }
+    
+        const id = req.params.id * 1;
+        const nationToDelete = nations.find((el) => el.id === id);
+    
+        if (!nationToDelete) {
+          return res.status(404).json({
+            status: "fail",
+            message: "No nation with ID " + id + " is found to delete",
+          });
+        }
+    
+        const index = nations.indexOf(nationToDelete);
+    
+        nations.splice(index, 1);
+    
+        fs.writeFile(`${__dirname}/../data/nations.json`, JSON.stringify(nations), (err) => {
+          if (err) {
+            return res.status(500).json({
+              status: "error",
+              message: "Failed to delete the nation",
+            });
+          }
+    
+          return res.status(203).json({
+            status: "success",
+            data: {
+              message: "delete success " + id,
+            },
+          });
+        });
+      } catch (error) {
+        console.error(error); // Log any unexpected errors
+        return res.status(500).json({
+          status: "error",
+          message: "An unexpected error occurred",
+        });
+      }
+  }
+
+nationRouter
+.route('/')
+.get(getAllNation)
+.post(createNation)
+.put(updateNationPut)
+.delete(deleteAllNations);
+
+nationRouter
+.route('/:id')
+.get(getNationById)
+.patch(updateNationPatch)
+.delete(deleteNationById)
+
+module.exports = nationRouter;
